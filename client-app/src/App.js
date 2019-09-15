20effee3b317a98caa1dad333fefcca95a0d69c0
Ravi Bharatbhai PATEL <RaviBharatbhai.PATEL@amadeus.com>
2019-09-15 17:50:10 +0530

    sliders added

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from './component/Navbar'
+import Sliders from './component/Sliders'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <br>
+      </br>
+      <br>
+      </br>
+      <Sliders/>
     </div>
   );
 }
---
+++ client-app/src/component/Sliders.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
+class Sliders extends Component {
+    render() {
+        return (
+            <div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={img1}
+                            alt="First Image"
+                            style={{height:"600px"}}
+                        />
+                        <Carousel.Caption>
+                            <h3>Want to make money?</h3>
+                            <p>We provide the best advice to make more money</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={img2}
+                            alt="Second Image"
+                            style={{height:"600px"}}
+                        />
+
+                        <Carousel.Caption>
+                            <h3 style={{color:"#060808"}}>Interested in Stockmarkets?</h3>
+                            <p style={{color:"#060808"}}>Connect with us and get the best benefits of stock markets</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={img3}
+                            alt="Third Image"
+                            style={{height:"600px"}}
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Retirement Plan?</h3>
+                            <p>Start Investing Early. The sooner, The better!</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        );
+    }
+}
+
+export default Sliders;
