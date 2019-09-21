6be0f2974942d55c753d1861db8e8847c02af2e3
Ravi Bharatbhai PATEL <RaviBharatbhai.PATEL@amadeus.com>
2019-09-21 15:29:01 +0530

    footer added

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './component/Navbar'
 import Sliders from './component/Sliders'
+import Footer from './component/Footer'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <br>
-      </br>
-      <br>
-      </br>
-      <Sliders/>
+      
+ <Sliders/>
+ <Footer/>
     </div>
   );
 }
